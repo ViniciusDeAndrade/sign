8d83170ff733f1f193cea773d807f2ad1d5af0ea
Vinicius de Andrade <viniciusclo@gmail.com>
2021-04-11 14:23:37 -0300

    post and get working. Datasource bean set

---
+++ sign/src/main/kotlin/br/com/sign/constants/DBConstants.kt
@@ -0,0 +1,7 @@
+package br.com.sign.constants
+
+const val DB_DRIVER = "com.mysql.jdbc.Driver"
+const val DB_DATABASE_NAME = "/syfly"
+const val DB_URL = "jdbc:mysql://localhost:3306"
+const val DB_USER = "root"
+const val DB_PASSWORD = ""
